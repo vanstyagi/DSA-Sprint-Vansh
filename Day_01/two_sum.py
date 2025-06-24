c3d4422f0c2cb36ce31c00da86ed864bf07d197f
vanstyagi <vanshtyagi722@gmail.com>
2025-06-24 10:54:25 +0530

    Day 01 ✅ Solved Two Sum, Valid Parentheses, Merge Two Sorted Lists
    Initial Leetcode push for DSA Python sprint - 6 Month Skill War Begins!

---
+++ Day_01/two_sum.py
@@ -0,0 +1,6 @@
+def twoSum(nums, target):
+    hashmap = {}
+    for i, num in enumerate(nums):
+        if target - num in hashmap:
+            return [hashmap[target - num], i]
+        hashmap[num] = i
